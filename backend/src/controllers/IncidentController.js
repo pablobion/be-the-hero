538c488ebf99fc45abe0eda1c3ab38f1178ec5b8
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2020-03-25 22:21:46 -0300

    Backend Finalizado

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,7 +3,25 @@ const connection = require('../database/connection')
 module.exports = {
 
     async index(request, response){
-        const incidents = await connection('incidents').select('*')
+        const {page = 1} = request.query // query params, se não existir vvai usar "1"
+
+        const [count] = await connection('incidents').count()
+
+        const incidents = await connection('incidents')
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+        .limit(5)
+        .offset((page-1)*5)
+        .select([
+            'incidents.*',
+            'ongs.name', 
+            'ongs.email', 
+            'ongs.whatsapp',
+            'ongs.city',
+            'ongs.uf'
+        ])
+
+        response.header('X-Total-Count', count['count(*)'])
+
         return response.json(incidents)
     },
    
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const routes = require('./routes')
-
+const cords = require('cors')
 
 const app = express()
+
+app.use(cors())
+
+// app.use(cors({    <<, se estive em produção precisamos colocar o origin
+//     origin: 'http://meuapp.com'
+// }))
+
 
 app.use(express.json())
 app.use(routes)
