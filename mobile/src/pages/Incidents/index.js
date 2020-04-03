09c422e6e0c1f09d6c2f61333b8de2051d3143e9
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2020-04-03 07:18:27 -0300

    Mobile pegando casos do backend, e mostrando total's

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Feather} from '@expo/vector-icons'
 import {View, FlatList, Image, Text, TouchableOpacity} from 'react-native'
 import logoImg from '../../assets/logo.png'
 import {useNavigation} from '@react-navigation/native'
 import styles from './styles'
+import api from '../../services/api'
+
+
 export default function Incidents(){
+    const [incidents, setIncidents] = useState([])
+    const [total, setTotal] = useState(0)
     const navigation = useNavigation()
 
     function navigateToDetail(){
         navigation.navigate('Detail')
     }
+
+    async function loadIncident(){
+        const response = await api.get('incidents')
+        setIncidents(response.data)
+        setTotal(response.headers['x-total-count'])
+    }
+
+    useEffect(() =>{
+        loadIncident()
+    }, [])
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg}></Image>
                 <Text style={styles.headerText}>
-                    Total de <Text style={styles.headerTextBold}>0 casos.</Text>
+                    Total de <Text style={styles.headerTextBold}>{total} casos.</Text>
                 </Text>
             </View>
         <Text style={styles.title}>Bem-Vindo!</Text>
@@ -23,19 +39,21 @@ export default function Incidents(){
 
         <FlatList 
             style={styles.incidentList} //passando o css para a incident list
-            data={[1,2,3]}
+            data={incidents}
             showsVerticalScrollIndicator={false} //removendo os scroll do lado
-            keyExtractor={incident => String(incident)} // key unica (o read pede isso)
-            renderItem={() => (
+            keyExtractor={incident => String(incident.id)} // key unica (o read pede isso)
+            renderItem={({item: incident}) => (
                 <View style={styles.incident}>
                 <Text style={styles.incidentProperty}>ONG:</Text>
-                <Text style={styles.incidentValue}>Apadsss</Text>
+                <Text style={styles.incidentValue}>{incident.name}</Text>
 
                 <Text style={styles.incidentProperty}>Caso:</Text>
-                <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+                <Text style={styles.incidentValue}>{incident.title}</Text>
 
                 <Text style={styles.incidentProperty}>Valor:</Text>
-                <Text style={styles.incidentValue}>R$ 120,00</Text>
+                <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', {
+                    style: 'currency', 
+                    currency: 'BRL' }).format(incident.value)}</Text>
 
                 <TouchableOpacity 
                     style={styles.detailsButton} 
