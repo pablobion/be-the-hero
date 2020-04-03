3dd3c29ecb272e5dbeda5ba75f2f4af599365250
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2020-04-03 07:52:29 -0300

    Mobile completo

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -11,19 +11,37 @@ export default function Incidents(){
     const [incidents, setIncidents] = useState([])
     const [total, setTotal] = useState(0)
     const navigation = useNavigation()
+    const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(false)
 
-    function navigateToDetail(){
-        navigation.navigate('Detail')
+    function navigateToDetail(incident){
+        navigation.navigate('Detail', {incident})
     }
 
-    async function loadIncident(){
-        const response = await api.get('incidents')
-        setIncidents(response.data)
+    async function loadIncidents(){
+        if(loading == true){
+            return
+        }
+
+        if (total > 0 && incidents.length == total){
+            return
+        }
+
+        setLoading(true)
+
+        const response = await api.get('incidents', {
+            params: {page}
+        })
+
+        
+        setIncidents([... incidents, ... response.data])
         setTotal(response.headers['x-total-count'])
+        setPage(page+1)
+        setLoading(false)
     }
 
     useEffect(() =>{
-        loadIncident()
+        loadIncidents()
     }, [])
 
     return (
@@ -40,24 +58,26 @@ export default function Incidents(){
         <FlatList 
             style={styles.incidentList} //passando o css para a incident list
             data={incidents}
-            showsVerticalScrollIndicator={false} //removendo os scroll do lado
+            //showsVerticalScrollIndicator={false} //removendo os scroll do lado
+            onEndReached={loadIncidents}
+            onEndReachedThreshold={0.2}
             keyExtractor={incident => String(incident.id)} // key unica (o read pede isso)
             renderItem={({item: incident}) => (
                 <View style={styles.incident}>
-                <Text style={styles.incidentProperty}>ONG:</Text>
-                <Text style={styles.incidentValue}>{incident.name}</Text>
+                    <Text style={styles.incidentProperty}>ONG:</Text>
+                    <Text style={styles.incidentValue}>{incident.name}</Text>
 
-                <Text style={styles.incidentProperty}>Caso:</Text>
-                <Text style={styles.incidentValue}>{incident.title}</Text>
+                    <Text style={styles.incidentProperty}>Caso:</Text>
+                    <Text style={styles.incidentValue}>{incident.title}</Text>
 
-                <Text style={styles.incidentProperty}>Valor:</Text>
-                <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', {
-                    style: 'currency', 
-                    currency: 'BRL' }).format(incident.value)}</Text>
+                    <Text style={styles.incidentProperty}>Valor:</Text>
+                    <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', {
+                        style: 'currency', 
+                        currency: 'BRL' }).format(incident.value)}</Text>
 
                 <TouchableOpacity 
                     style={styles.detailsButton} 
-                    onPress={navigateToDetail}
+                    onPress={() => navigateToDetail(incident)}
                 >
                     <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
                     <Feather name="arrow-right" size={16} color="#E02141"/>
